904ce6861993aef455e821ba4e7c0acd9b4b8064
chLi <565353780@qq.com>
2022-06-29 20:38:36 +0800

    update cmds

--- test.sh
+++ test.sh
@@ -14,9 +14,9 @@
 
 # railway
 
-./darknet detector test \
-  Config/railway/voc.data \
-  Config/railway/yolov3.cfg \
-  Config/railway/yolov3_train_2c_detect_2class.backup \
-  /home/chli/chLi/Download/DeepLearning/Dataset/RailwayStation/2C_mask/train_dataset/0.jpg
+# ./darknet detector test \
+  # Config/railway/voc.data \
+  # Config/railway/yolov3.cfg \
+  # Config/railway/yolov3_train_2c_detect_2class.backup \
+  # /home/chli/chLi/Download/DeepLearning/Dataset/RailwayStation/2C_mask/train_dataset/0.jpg
 
--- train.sh
+++ train.sh
@@ -1,5 +1,14 @@
-./darknet detector train \
-  cfg/voc.data \
-  cfg/yolov3-tiny.cfg \
-  darknet53.conv.74
+# waterdrop
+
+# ./darknet detector train \
+  # Config/waterdrop/voc.data \
+  # Config/waterdrop/yolov3-tiny.cfg \
+  # darknet53.conv.74
+
+# railway
+
+# ./darknet detector train \
+  # Config/railway/voc.data \
+  # Config/railway/yolov3.cfg \
+  # darknet53.conv.74
 
