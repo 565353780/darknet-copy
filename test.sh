2623801f7a4ac4c2e3817b76fba3580606a662aa
chLi <565353780@qq.com>
2022-06-29 20:37:18 +0800

    update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,22 @@
-./darknet detector test \
-  cfg/voc.data \
-  cfg/yolov3-tiny.cfg \
-  backup/yolov3-tiny_92000_2step_detect.weights \
-  /home/chli/waterdrop_data/1_2048x1440_nv21/first_merge/0.png
+# waterdrop
 
 # ./darknet detector test \
-  # cfg/voc.data \
-  # cfg/yolov3-tiny.cfg \
-  # backup/yolov3-tiny_92000_2step_detect.weights \
+  # Config/waterdrop/voc.data \
+  # Config/waterdrop/yolov3-tiny.cfg \
+  # Config/waterdrop/yolov3-tiny_92000_2step_detect.weights \
+  # /home/chli/waterdrop_data/1_2048x1440_nv21/first_merge/0.png
+
+# ./darknet detector test \
+  # Config/waterdrop/voc.data \
+  # Config/waterdrop/yolov3-tiny.cfg \
+  # Config/waterdrop/yolov3-tiny_92000_2step_detect.weights \
   # /home/chli/waterdrop_data/1_2048x1440_nv21/merge/0.png
+
+# railway
+
+./darknet detector test \
+  Config/railway/voc.data \
+  Config/railway/yolov3.cfg \
+  Config/railway/yolov3_train_2c_detect_2class.backup \
+  /home/chli/chLi/Download/DeepLearning/Dataset/RailwayStation/2C_mask/train_dataset/0.jpg
 
